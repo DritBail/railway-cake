102a02dab2e4b50834dcf834da6da7fe841cc16c
DritBail <a4le516db@hotmail.com>
2026-05-18 08:35:59 -0600

    No se
    Agregue archivos y asi todo guay

---
+++ config/import sys.py
@@ -0,0 +1,8 @@
+import sys
+print("--------------------------------------------------")
+print("¡Felicidades! Si puedes leer esto, Python funciona.")
+print(f"Versión instalada: {sys.version}")
+print("--------------------------------------------------\n")
+# Pedimos un dato para probar la interacción en la terminal
+nombre = input("Por favor, escribe tu nombre y presiona Enter: ")
+print(f"\n¡Excelente, {nombre}! Todo está listo en tu VS Code paraempezar a programar.")
